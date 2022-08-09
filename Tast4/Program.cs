421033ed84a2b450ad0df3891c4cb7ca6652b7db
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-09 16:31:46 +0300

    Добавили запись

--- Tast4/Program.cs
+++ Tast4/Program.cs
@@ -5,7 +5,7 @@
 //782 -> 2
 //918 -> 8
 
-Console.WriteLine("Введите чило: ");
+Console.WriteLine("Введите трехзначное чило: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = num / 10;
 count = num % count;
