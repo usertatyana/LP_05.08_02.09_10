d0603d9b74a225fd5324911604c04db7d1449640
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-12 15:58:08 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая
+//выводит случайное трех значное число, и
+//удаляет вторую цифру этого числа
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+int  RemoveSecond(int num);
+{
+   int number1 = num / 100;
+   int number2 = num % 10;
+   int nov = number1 * 10 + number2;
+   return nov;
+}
+
+int result = RemoveSecond(number);
+Console.WriteLine($"{number} -> {result}");
