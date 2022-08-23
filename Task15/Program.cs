0c50aa8e4774a291e011356dd9fc73a27a2381ab
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-23 19:23:33 +0300

    Домашняя работа 19

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая 
+//принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void ArrayDigit(string number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+{
+    ArrayDigit(number);
+}
+else Console.WriteLine($"Введи правильное число");
