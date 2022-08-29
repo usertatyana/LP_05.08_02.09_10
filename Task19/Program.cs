26eab0b3ae48d8a6bd942c52b9075fb059803919
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-29 12:48:59 +0300

    Задача 24

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая 
+//принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+//Summa = 0;
+//for(i = 0; i < n; i++)
+//Summa = Summa + A[i];
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Сумма чисел {n} + {n} + {n} = {sum}");
+
---
+++ Task22/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу которая,
+// принимает на вход число A и
+// выдает сумму чисел
+// 7-> 28
+//4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// for (int i = 1; i <= num; i++)
+// {
+    // sum = sum + i;
+   // sum += i;
+// }
+
+//Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return  sum;
+}
+
+int result = SumNumbers(num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {result}");
