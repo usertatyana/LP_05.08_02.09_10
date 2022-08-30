b9950571064320bd3a644ba56ea91c8af41127d6
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-30 10:01:19 +0300

    Домашняя работа 29

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Задача 29: Напишите программу, 
+//которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите размер массива: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+int[] RandArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+int[] res = RandArray(lenght);
+
+void PrintArray(int[] res1)
+{
+    Console.Write($" " );
+    for (int i = 0; i < res1.Length; i++)
+    {
+        Console.Write($"{res1[i]},");
+    }
+    Console.Write($" ");
+}
+PrintArray(res);
