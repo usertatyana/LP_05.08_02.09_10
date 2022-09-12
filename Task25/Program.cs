edc79fe04f8b35619f95504163c19efa63675128
Tatyana Kapina <deniscapin@yandex.ru>
2022-09-12 14:09:17 +0300

    Задача 31 на уроку

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -37,7 +37,7 @@ void PrintArray(int[] array)
     Console.Write("[ ");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i < array.Length - 1) Console.Write($"{array[i]}");
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
         else Console.Write(array[i]);
     }
     Console.WriteLine("]");
